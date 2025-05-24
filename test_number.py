8d20f534bda8499df6fc60d7164f7ffaa8ee4916
Chuyue291 <chuyue202505@163.com>
2025-05-24 14:20:57 +0800

    Update test_number.py

--- test_number.py
+++ test_number.py
@@ -1,5 +1,6 @@
 from number_class import number
 
 def test_number_oerators()  -> None:
-    a=number(15,8,9,7.7)
+    a=number(15,8,9,7.7),
     assert a.is_even() == (False,True,False,False)
+    assert a.is_odd() == (True,False,True,False)
